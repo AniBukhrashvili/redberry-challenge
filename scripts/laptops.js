5ced00ccf1942526938c5b5cfd9e8b35e83331ce
AniBukhrashvili <ana.bukhrashvili.1@btu.edu.ge>
2022-09-03 20:21:12 +0400

    second form validation

--- scripts/laptops.js
+++ scripts/laptops.js
@@ -1,11 +1,15 @@
-const photoContent = document.querySelector('.photo-content');
-const laptopName = document.querySelector('#laptop-name-label');
+const laptopName = document.querySelector('#laptop-name');
 const laptopBrand = document.querySelector('#laptop-brand');
 const cpu = document.querySelector('#cpu');
-const cpuCore = document.querySelector('#cpu-cores-label');
-const cpuThread = document.querySelector('#cpu-threads-label');
-const ram = document.querySelector('#ram-label');
-const laptopPrice = document.querySelector('#price-label');
+const cpuCore = document.querySelector('#cpu-cores');
+const cpuThread = document.querySelector('#cpu-threads');
+const ram = document.querySelector('#ram');
+const date = document.querySelector('#date');
+const ssd = document.querySelector('#ssd');
+const hdd = document.querySelector('#hdd');
+const newLap = document.querySelector('#new');
+const secondary = document.querySelector('#secondary');
+const laptopPrice = document.querySelector('#price');
 
 
 // fetch brands&cpus API
@@ -56,4 +60,65 @@ brandSelect.onclick = () => {
 
 cpuSelect.onclick = () => {
     cpuSelect.classList.toggle('active');
+}
+
+
+// second form validation
+
+const saveButton = document.querySelectorAll('#form .save-btn')[0];
+
+saveButton.addEventListener('click', (event) => {
+    checkValidation();
+})
+
+const checkName = (input) => {
+    let nameRegex = /^[a-z0-9!@#$%^&*()_+=]+$/i;
+    if (!nameRegex.test(input.value)) {
+        input.parentElement.classList.add('error');
+    } else {
+        input.parentElement.classList.remove('error');
+    }
+}
+
+const checkSelect = (input) => {
+    if (input.value.length === 0) {
+        input.parentElement.classList.add('error');
+    } else {
+        input.parentElement.classList.remove('error');
+    }
+}
+
+const checkNum = (input) => {
+    const numberRegex = /^[0-9]+$/;
+    if (!numberRegex.test(input.value)) {
+        input.parentElement.classList.add('error');
+    } else {
+        input.parentElement.classList.remove('error');
+    }
+}
+
+const checkRadio = (radio) => {
+    const elements = document.querySelectorAll(`.form-control input`);
+    elements.forEach(input => {
+        if (input.checked = false) {
+            radio.parentElement.classList.add('error');
+        } else {
+            radio.parentElement.classList.remove('error');
+        }
+    })
+}
+
+
+const checkValidation = () => {
+    checkName(laptopName);
+    checkSelect(laptopBrand);
+    checkSelect(cpu);
+    checkNum(cpuCore);
+    checkNum(cpuThread);
+    checkNum(ram);
+    checkNum(laptopPrice);
+    checkRadio(ssd);
+    checkRadio(hdd);
+    checkRadio(newLap);
+    checkRadio(secondary);
 }
